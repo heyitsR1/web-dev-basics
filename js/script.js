e1cea91d6d89572d70da8f0ab386ab0c77a3d2ed
Aarohan Niraula <aarohan567@gmail.com>
2025-05-21 10:45:41 +0545

    Week 3 of JS fundamentals

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Week 1: 
+//     Learnt about console log 
+//     variable declaration 
 console.log ("This is external file")
 let x = "Rita Rimal";
 console.log(x)
@@ -13,6 +16,10 @@ console.log ("Sum is ", sum)
 console.log ("Difference is ", difference)
 console.log ("Product is ", product)
 
+// Week 2: 
+//     Learnt about if conditions
+//     Learnt about loops
+
 let year = 2080
 
 if (year == 2082){
@@ -22,7 +29,6 @@ if (year == 2082){
 } else {
     console.log (year + " has already passed")
 }
-
 
 let num = prompt ("Table of which number : ")
 for (let i = 1; i <= 10; i++) {
@@ -37,3 +43,20 @@ while (j < 20) {
     console.log("Odd Number is ", j)
     j+=1
 }
+alert("Product is " + product_function(a));
+
+
+// Week 3: 
+//     Learnt about functions, hoisting
+//     Learnt about arguments, default parameters, 
+
+
+let z = prompt ("Enter number")
+function table_function (x = 0){
+    for (let i = 1; i <= 10; i++) {
+        console.log (`${z} x ${i} = ${z*i}`)
+        
+    }
+}
+table_function(a)
+// hoisting, js engine keeps all var and functions at top of block (code) for execution 
